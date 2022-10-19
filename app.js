cb082bd4894aa1946728c1a727f932725fa2ce46
Shoaib Zeshan <szeeshan707@gmail.com>
2022-10-19 23:19:29 +0530

    adding more functionalities

--- app.js
+++ app.js
@@ -2,79 +2,38 @@ const billInput = document.querySelector('.bill-input')
 const customInput = document.querySelector('.custom-input')
 const countInput = document.querySelector('.number-of-person')
 const btns = document.querySelectorAll('.btn')
-const tipPerPerson = document.querySelector('.tip_amount')
+const tipDivided = document.querySelector('.tip_amount')
 const totalAmount = document.querySelector('.total_amount')
 const resetBtn = document.querySelector('.reset-btn')
 const form = document.querySelector('form')
 const customTip = document.querySelector('.custom-input')
 
-let total = 0
-let tip = 0
-let bill = 0
-let people = null
-resetBtn.disable = true
-
-const tipCalc = (e) => {
-    if ((bill != 0 && bill != null) && (people = ! null && people != 0)) {
-        totalAmount.textContent = `$${((bill + (bill / 100 * tip)) / people).toFixed(2)}`
-        tipPerPerson.textContent = `$${((bill / 100 * tip) / people).toFixed(2)}`
-    }
-}
+// let total = 0
+// let tip = 0
+// let bill = 0
+// let people = null
+// resetBtn.disable = true
 
 
-const billInputUpdate = (e) => {
-    let value = parseFloat(e.target.value)
-    if (value < 0) {
-        e.target.value = null
-
-    } else {
-        bill = value
-    }
-    tipCalc()
-
-}
-
-const peopleInputFun = (e) => {
-    // error.textContent = "";
-    let value = parseInt(e.target.value);
-    if (value < 0) {
-        e.target.value = null
-
-    } else {
-        people = value
-    }
-    tipCalc()
+console.log(customInput)
 
 
-}
+const calculateTip = (e) => {
+    let billValue = parseFloat(billInput.value)
+    let customValue = parseFloat(customInput.value)
+    billInput.value = billValue.toFixed(2)
 
-const customTipFun = (e) => {
-    let value = parseFloat(e.target.value)
-    if (value < 0) {
-        e.target.value = null
+    btns.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            const btnValue = e.target.value
+            let totalTip = parseFloat((billValue * (btnValue / 100)).toFixed(2))
+            let total = parseFloat((billValue + totalTip).toFixed(2))
 
-    } else {
-        tip = value
-    }
-    if (tip < 0)
-        e.target.value = null
-    tipCalc()
+            let tipPerPerson = (totalTip / countInput.value).toFixed(2)
+            let totalPerPerson = (total / countInput.value).toFixed(2)
 
-
-}
-
-
-
-billInput.addEventListener('change', billInputUpdate)
-countInput.addEventListener('change', peopleInputFun)
-customInput.addEventListener('change', customTipFun)
-
-
-
-btns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        const value = e.target.innerText
-        console.log(value)
+            const tipDivided = document.querySelector('.tip_amount').textContent = tipPerPerson;
+            const totalAmount = document.querySelector('.total_amount').textContent = totalPerPerson
 
 
 
@@ -82,79 +41,31 @@ btns.forEach((btn) => {
 
 
 
+        })
     })
+
+
+}
+
+
+billInput.addEventListener('change', calculateTip)
+resetBtn.addEventListener('click', () => {
+    billInput.value = ''
+    countInput.value = ''
+    const tipDivided = document.querySelector('.tip_amount').textContent = 0;
+    const totalAmount = document.querySelector('.total_amount').textContent = 0;
+
+
+
+
+
 })
 
+// countInput.addEventListener('change', calculateTip)
 
 
 
 
-// billInput.addEventListener('input', billInputFun)
-// countInput.addEventListener('input', peopleInputFun)
 
-// btns.forEach((btn) => {
-//     btn.addEventListener('click', handleClick)
-// })
-
-// customTip.addEventListener('click', customTipFun)
-// resetBtn.addEventListener('click', reset)
-
-
-// billInput.value = "";
-// countInput.value = "";
-// tipPerPerson.innerHTML = "$" + (0.0).toFixed(2)
-// totalAmount.innerHTML = "$" + (0.0).toFixed(2)
-
-// let billValue = 0.0;
-// let peopleValue = 1;
-// let tipValue = 0.15;
-
-// function billInputFun() {
-//     billValue = parseFloat(billInput.value)
-//     calculateTip()
-
-
-// }
-// function peopleInputFun() {
-//     peopleValue = parseFloat(countInput.value)
-
-//     calculateTip()
-
-// }
-
-
-// function customTipFun() {
-//     tipValue = parseFloat(customTip.value / 100);
-//     calculateTip()
-// }
-// function handleClick(e) {
-//     btns.forEach((btn) => {
-//         if (e.target.innerHTML === btn.innerHTML) {
-//             tipValue = parseFloat(btn.innerHTML) / 100
-//         }
-//     })
-//     calculateTip()
-
-// }
-
-// function calculateTip() {
-//     if (peopleValue >= 0) {
-//         let tipAmount = (billValue * tipValue) / peopleValue
-//         let total = (billValue * tipAmount) / peopleValue
-//         tipPerPerson.innerHTML = "$" + tipAmount.toFixed(2)
-//         totalAmount.innerHTML = "$" + total.toFixed(2)
-
-//     }
-// }
-
-// function reset() {
-//     billInput.value = "";
-//     // billInputFun()
-//     countInput.value = "";
-//     // peopleInputFun()
-//     customTip.value = "";
-//     countInput.value = "";
-
-// }
 
 
